4699476d679bf67bd5fbf297f3cf3d69c6bc8df4
sujin <orego.jin@gmail.com>
2021-10-07 14:02:30 -0400

    practice01 updated

--- web-scraper/practice01.py
+++ web-scraper/practice01.py
@@ -21,3 +21,18 @@ def power(a,b):
 
 def negation(a):
   return -a
+
+
+def age_check(age):
+  print(f"you are {age} years old")
+  if age < 18:
+    print("you are too young")
+  elif age == 18 or age ==19:
+    print("you are new!")
+  elif age >20 and age < 25:
+    print("you are kind of young")
+  else:
+    print("enjoy your drink")
+
+age_check(21)
+
